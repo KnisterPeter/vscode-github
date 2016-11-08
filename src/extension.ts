db195b74ed7594dc7bd11637376ca6b6f20661ce
Markus Wolf <mail@markus-wolf.de>
2016-11-08 21:04:40 +0100

    feat(vscode-github): list pull-requests

--- src/extension.ts
+++ src/extension.ts
@@ -1,64 +1,59 @@
 'use strict';
 import * as vscode from 'vscode';
-import {Pretend, Get, Post} from 'pretend';
+import {getClient, GitHub} from './github';
 
-class GitHub {
-    @Get('/user/repos')
-    public async listRepositories(): Promise<GitHubResonse<Repository[]>> { return undefined; }
+let github: GitHub;
 
-    @Post('/repos/{owner}/{repo}/pulls')
-    public async createPullRequest(owner: string, repo: string, body: any) { return undefined; }
+export function activate(context: vscode.ExtensionContext): void {
+  getToken(context).then(token => {
+    github = getClient(token);
+  });
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand('extension.setGitHubToken',
+      createGithubTokenCommand(context)));
+  context.subscriptions.push(
+    vscode.commands.registerCommand('extension.listPullRequests',
+      listPullRequests));
 }
 
-interface GitHubResonse<T> {
-    headers: any;
-    body: T;
-}
-
-interface Repository {
-    id: number;
-    owner: any;
-    name: string;
-    full_name: string;
-    default_branch: string;
-}
-
-export function activate(context: vscode.ExtensionContext) {
-    let token = context.globalState.get<string>('key');
-    if (!token) {
-        vscode.window.showInputBox({
-            ignoreFocusOut: true,
-            password: true,
-            placeHolder: 'GitHub Personal Access Token'
-        }).then(input => {
-            context.globalState.update('key', input);
-            token = input;
-        });
-    }
-    const github = Pretend
-        .builder()
-        .requestInterceptor(request => {
-            request.options.headers['Authorization'] = `token ${token}`;
-            return request;
-        })
-        .decode(async response => {
-            return {
-                headers: response.headers,
-                body: await response.json()
-            };
-        })
-        .target(GitHub, 'https://api.github.com')
-
-    let disposable = vscode.commands.registerCommand('extension.sayHello', async () => {
-        vscode.window.showInformationMessage('Hello World!');
-        console.log('request...');
-        const reponse = await github.listRepositories();
-        console.log('...done');
-        console.log('repos', reponse.headers, reponse.body[0]);
+function getToken(context: vscode.ExtensionContext): PromiseLike<string> {
+  let token = context.globalState.get<string>('token');
+  if (token) {
+    return Promise.resolve(token);
+  }
+  return createGithubTokenCommand(context)()
+    .then(() => {
+      return Promise.resolve(context.globalState.get<string>('token'));
     });
-
-    context.subscriptions.push(disposable);
 }
 
-export function deactivate() {
+function createGithubTokenCommand(context: vscode.ExtensionContext): () => PromiseLike<void> {
+  return () => {
+    const options = {
+      ignoreFocusOut: true,
+      password: true,
+      placeHolder: 'GitHub Personal Access Token'
+    };
+    return vscode.window.showInputBox(options)
+      .then(input => context.globalState.update('token', input))
+      .then(() => getToken(context))
+      .then(token => {
+        github = getClient(token);
+      });
+  };
+}
+
+async function listPullRequests(): Promise<void> {
+  if (!github) {
+    setTimeout(() => listPullRequests(), 1000);
+    return;
+  }
+  try {
+    const reponse = await github.listPullRequests('KnisterPeter', 'vscode-github');
+    console.log('repos', reponse.body);
+  } catch (err) {
+    console.error(err);
+    vscode.window.showErrorMessage('Failed to execute GitHub request');
+  }
 }
---
+++ src/github.ts
@@ -0,0 +1,58 @@
+import {Pretend, Get, Post, IPretendRequestInterceptor, IPretendDecoder} from 'pretend';
+
+export function getClient(token: string): GitHub {
+  return Pretend
+    .builder()
+    .requestInterceptor(githubTokenAuthenticator(token))
+    .decode(githubDecoder())
+    .target(GitHub, 'https://api.github.com');
+}
+
+function githubTokenAuthenticator(token: string): IPretendRequestInterceptor {
+  return request => {
+    if (!request.options.headers) {
+      request.options.headers = {};
+    }
+    request.options.headers['Authorization'] = `token ${token}`;
+    return request;
+  };
+}
+
+function githubDecoder(): IPretendDecoder {
+  return async response => {
+    if (response.status >= 400) {
+      const err = new Error('GitHub request failed');
+      (err as any).response = response;
+      throw err;
+    }
+    return {
+      headers: response.headers,
+      body: await response.json()
+    };
+  };
+}
+
+export class GitHub {
+
+  @Get('/repos/{owner}/{repo}/pulls')
+  public async listPullRequests(_owner: string, _repo: string):
+    Promise<GitHubResonse<PullRequest[]>> { return undefined as any; }
+
+  @Post('/repos/{owner}/{repo}/pulls')
+  public async createPullRequest(_owner: string, _repo: string, _body: any):
+    Promise<void> { return undefined as any; }
+
+}
+
+interface GitHubResonse<T> {
+  headers: any;
+  body: T;
+}
+
+interface PullRequest {
+  id: number;
+  number: number;
+  state: 'open' | 'closed';
+  title: string;
+  body: string;
+}
