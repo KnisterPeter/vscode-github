fc8fed40c55925e46b12c4959f62817d2835000a
Markus Wolf <mail@markus-wolf.de>
2016-11-15 19:10:01 +0100

    refactor: further simplifications

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,7 @@ import {join} from 'path';
 import * as sander from 'sander';
 import * as vscode from 'vscode';
 import * as git from './git';
-import {GitHubError} from './github';
+import {GitHubError, PullRequest} from './github';
 import {StatusBarManager} from './status-bar-manager';
 import {GitHubManager} from './github-manager';
 
@@ -21,24 +21,18 @@ export function activate(context: vscode.ExtensionContext): void {
   githubManager = new GitHubManager(cwd, channel);
   statusBarManager = new StatusBarManager(context, githubManager);
 
-  const token = getToken(context);
+  const token = context.globalState.get<string|undefined>('token');
   if (token) {
     githubManager.connect(token);
   }
   checkVersionAndToken(context, token);
 
   context.subscriptions.push(
-    vscode.commands.registerCommand('extension.setGitHubToken',
-      createGithubTokenCommand(context)));
-  context.subscriptions.push(
-    vscode.commands.registerCommand('extension.createPullRequest',
-      wrapCommand(createPullRequest)));
-  context.subscriptions.push(
-    vscode.commands.registerCommand('extension.checkoutPullRequests',
-      wrapCommand(checkoutPullRequests)));
-  context.subscriptions.push(
-    vscode.commands.registerCommand('extension.browserPullRequest',
-      wrapCommand(browserPullRequest)));
+    vscode.commands.registerCommand('extension.setGitHubToken', createGithubTokenCommand(context)),
+    vscode.commands.registerCommand('extension.createPullRequest', wrapCommand(createPullRequest)),
+    vscode.commands.registerCommand('extension.checkoutPullRequests', wrapCommand(checkoutPullRequests)),
+    vscode.commands.registerCommand('extension.browserPullRequest', wrapCommand(browserPullRequest))
+  );
 }
 
 function checkVersionAndToken(context: vscode.ExtensionContext, token: string|undefined): void {
@@ -58,17 +52,17 @@ function checkVersionAndToken(context: vscode.ExtensionContext, token: string|un
 function wrapCommand<T>(command: T): T {
   const wrap: any = (...args: any[]) => {
     if (githubManager.connected && cwd) {
-      return (command as any).apply(null, args);
+      try {
+        return (command as any).apply(null, args);
+      } catch (e) {
+        logAndShowError(e);
+      }
     } else {
       vscode.window.showWarningMessage('Please setup your Github Personal Access Token '
         + 'and open a GitHub project in your workspace');
     }
   };
   return wrap;
-}
-
-function getToken(context: vscode.ExtensionContext): string|undefined {
-  return context.globalState.get<string|undefined>('token');
 }
 
 function logAndShowError(e: Error): void {
@@ -97,50 +91,35 @@ function createGithubTokenCommand(context: vscode.ExtensionContext): () => Promi
 }
 
 async function createPullRequest(): Promise<void> {
-  try {
-    const pullRequest = await githubManager.createPullRequest();
-    if (pullRequest) {
-      statusBarManager.updatePullRequestStatus(true);
-      vscode.window.showInformationMessage(`Successfully created #${pullRequest.number}`);
-    }
-  } catch (e) {
-    logAndShowError(e);
+  const pullRequest = await githubManager.createPullRequest();
+  if (pullRequest) {
+    statusBarManager.updatePullRequestStatus(true);
+    vscode.window.showInformationMessage(`Successfully created #${pullRequest.number}`);
   }
+}
+
+async function selectPullRequest(doSomething: (pullRequest: PullRequest) => void): Promise<void> {
+  const pullRequests = await githubManager.listPullRequests();
+  vscode.window.showQuickPick(pullRequests.map(pullRequest => ({
+    label: pullRequest.title,
+    description: `#${pullRequest.number}`,
+    pullRequest
+  }))).then(selected => {
+    if (selected) {
+      doSomething(selected.pullRequest);
+    }
+  });
 }
 
 async function checkoutPullRequests(): Promise<void> {
-  try {
-    const pullRequests = await githubManager.listPullRequests();
-    vscode.window.showQuickPick(pullRequests.map(pullRequest => ({
-      label: pullRequest.title,
-      description: `#${pullRequest.number}`,
-      pullRequest
-    }))).then(selected => {
-      if (selected) {
-        git.checkout(cwd, selected.pullRequest.head.ref)
-          .then(() => {
-            statusBarManager.updatePullRequestStatus();
-          });
-      }
-    });
-  } catch (e) {
-    logAndShowError(e);
-  }
+  selectPullRequest(async pullRequest => {
+    await git.checkout(cwd, pullRequest.head.ref);
+    statusBarManager.updatePullRequestStatus();
+  });
 }
 
 async function browserPullRequest(): Promise<void> {
-  try {
-    const response = await githubManager.listPullRequests();
-    vscode.window.showQuickPick(response.map(pullRequest => ({
-      label: pullRequest.title,
-      description: `#${pullRequest.number}`,
-      pullRequest
-    }))).then(selected => {
-      if (selected) {
-        vscode.commands.executeCommand('vscode.open', vscode.Uri.parse(selected.pullRequest.html_url));
-      }
-    });
-  } catch (e) {
-    logAndShowError(e);
-  }
+  selectPullRequest(pullRequest => {
+    vscode.commands.executeCommand('vscode.open', vscode.Uri.parse(pullRequest.html_url));
+  });
 }
