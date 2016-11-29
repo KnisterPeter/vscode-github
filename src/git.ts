6a9cc23f3fe7e5c146b1ac7d3eda87394d3cbad2
Markus Wolf <mail@markus-wolf.de>
2016-11-29 21:08:53 +0100

    fix: github urls could contain colon
    valid github urls could as well contain a colon to separate path (e.g.
    
    git@github.com:knisterpeter/vscode-github.git)

--- src/git.ts
+++ src/git.ts
@@ -2,25 +2,34 @@ import * as execa from 'execa';
 
 import { parse } from 'url';
 
+/**
+ * Look in .git/config for a remote origin and parses it to get username and repository.
+ *
+ * @param {string} cwd The directory to find the .git/config file in.
+ * @return {Promise<string[]>} A tuple of username and repository (e.g. KnisterPeter/vscode-github)
+ * @throws Throws if the could not be parsed as a github url
+ */
 export async function getGitHubOwnerAndRepository(cwd: string): Promise<string[]> {
-  // As we expect this function to throw on non-Github repos we can chain whatever calls and they will thrown on non-correct remotes
+  // as we expect this function to throw on non-Github repos we can chain
+  // whatever calls and they will thrown on non-correct remotes
   const remote = (await execa('git', 'config --local --get remote.origin.url'.split(' '), {cwd})).stdout.trim();
   if (!remote.length) {
     throw new Error('Git remote is empty!');
   }
 
-  // Git protocol remotes, may be git@github or git://github, GITHUB domain name is not case-sensetive
-  if(remote.startsWith('git')) {
-    return remote.match(/^git(@|\:\/\/)github.com\/(.*?)\/(.*?)(?:.git)?$/i).slice(2, 4);
+  // git protocol remotes, may be git@github:username/repo.git
+  // or git://github/user/repo.git, GITHUB domain name is not case-sensetive
+  if (remote.startsWith('git')) {
+    return remote.match(/^git(?:@|\:\/\/)github.com[\/:](.*?)\/(.*?)(?:.git)?$/i)!.slice(1, 3);
   }
 
   // it must be http or https based remote
   const { hostname, pathname } = parse(remote);
-  // Github.com domain name is not case-sensetive
+  // github.com domain name is not case-sensetive
   if (!pathname || !hostname || !/^github\.com$/i.test(hostname)) {
     throw new Error('Not a Github remote!');
   }
-  return pathname.match(/\/(.*?)\/(.*?)(?:.git)?$/).slice(1, 3);
+  return pathname.match(/\/(.*?)\/(.*?)(?:.git)?$/)!.slice(1, 3);
 }
 
 export async function getCurrentBranch(cwd: string): Promise<string|undefined> {
