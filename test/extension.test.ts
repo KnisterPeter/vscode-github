377416d9e830603390fff691240e0e4c76a51969
Markus Wolf <mail@markus-wolf.de>
2017-05-22 11:27:06 +0200

    fix: parsing git url
    Closes #107

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -4,6 +4,7 @@ import * as assert from 'assert';
 // as well as import your extension to test it
 import * as vscode from 'vscode';
 // import * as myExtension from '../src/extension';
+import * as git from '../src/git';
 
 suite('vscode-github extension tests', () => {
   test('Extension should be active after startup', () => {
@@ -12,12 +13,26 @@ suite('vscode-github extension tests', () => {
     assert.equal(extension!.isActive, true);
   });
 
-  test('Extension should register commands', done => {
+  test('should register commands', done => {
     vscode.commands.getCommands(true)
       .then(commands => commands.filter(command => command.startsWith('vscode-github')))
       .then(commands => {
         assert.equal(commands.length > 0, true);
       })
       .then(() => done());
+  });
+
+  test('should parse username and repository from github ssh url', () => {
+    const [host, user, repo] = git.parseGithubUrl('git@github:username/repo.git');
+    assert.equal(host, 'github');
+    assert.equal(user, 'username');
+    assert.equal(repo, 'repo');
+  });
+
+  test('should parse username and repository from github ssh:// url', () => {
+    const [host, user, repo] = git.parseGithubUrl('git://github/username/repo.git');
+    assert.equal(host, 'github');
+    assert.equal(user, 'username');
+    assert.equal(repo, 'repo');
   });
 });
